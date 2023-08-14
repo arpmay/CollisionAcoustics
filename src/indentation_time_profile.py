25ca3986b722363f93312c657597dfcc7cd505de
Arpit Sen Shrivastava <arpmay@gmail.com>
2023-08-14 12:28:52 +0530

    Create indentation_time_profile.py

---
+++ src/indentation_time_profile.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Constants
+R = 0.5 * 0.025  # 2.5 cm diameter ball, so radius = 1.25 cm = 0.0125 m
+E_star = 120e9 / (2 * (1 - 0.34**2))  # Using the earlier combined modulus of elasticity formula
+k = (4/3) * np.sqrt(R) * E_star
+delta_max = 4.42e-5  # from our previous calculations
+t_max = 2.38e-4  # from our previous calculations
+
+# Time array
+t = np.linspace(0, 2*t_max, 1000)  # twice t_max to cover the full trapezoidal profile
+
+# Indentation over time
+delta_t = delta_max * (t/t_max - 0.5 * (t/t_max)**2)
+
+# Plotting
+plt.figure(figsize=(10,6))
+plt.plot(t, delta_t)
+plt.title("Indentation over Time")
+plt.xlabel("Time (s)")
+plt.ylabel("Indentation (m)")
+plt.grid(True)
+plt.show()
